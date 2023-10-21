6d8996e470931174aa8a825bc7b996a365951680
callisto-jovy <romanportius@gmail.com>
2023-10-21 17:47:01 +0200

    Improvements to the audio analysis.

--- lib/main.dart
+++ lib/main.dart
@@ -81,6 +81,7 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
+
   final String title;
 
   @override
--- lib/widgets/time_stamp_painer.dart
+++ lib/widgets/time_stamp_painer.dart
@@ -1,26 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:media_kit_video/media_kit_video_controls/src/controls/extensions/duration.dart';
 
 class TimeStampPainter extends CustomPainter {
   //
 
-  final List<double> timeStamps;
-  final int audioLength;
+  final double width = 2.5;
 
-  TimeStampPainter(this.timeStamps, this.audioLength);
+  final List<double> timeStamps;
+  final double audioLength;
+  final Offset? hitOffset;
+
+  final Function(double) newTimeStamp;
+  final Function(List<double>) hitTimeStamp;
+
+  TimeStampPainter(
+      {super.repaint,
+      required this.timeStamps,
+      required this.audioLength,
+      required this.hitOffset,
+      required this.newTimeStamp,
+      required this.hitTimeStamp});
 
   @override
   void paint(Canvas canvas, Size size) {
     final paint = Paint()
       ..color = Colors.red
-      ..strokeWidth = 2
+      ..strokeWidth = width
       ..strokeCap = StrokeCap.square;
 
+    final List<double> markedForRemoval = [];
+
     for (final double timeStamp in timeStamps) {
-      final Offset p0 = Offset(timeStamp * size.width / audioLength, 0);
-      final Offset p1 = Offset(timeStamp * size.width / audioLength, size.height);
+      // dx = duration * width / length
+      final double dx = timeStamp * size.width / audioLength;
+      final Offset p0 = Offset(dx, 0);
+      final Offset p1 = Offset(dx, size.height);
+
+      // intersection hit with the offset, remove the timestamp
+
+      if (hitOffset != null && (dx - hitOffset!.dx).abs() <= 2) {
+        markedForRemoval.add(timeStamp);
+      } else {
+        canvas.drawLine(p0, p1, paint);
+
+        final TextSpan span = TextSpan(text: Duration(milliseconds: timeStamp.round()).label());
+        final TextPainter tp = TextPainter(text: span, textDirection: TextDirection.ltr);
+        tp.layout();
+        tp.paint(canvas, p1.translate(-tp.width * 0.5, 0)); // Center text
+      }
+    }
+
+    // add new timestamp if no timestamps were hit & a hit offset exists
+    if (markedForRemoval.isEmpty && hitOffset != null) {
+      final double dx = hitOffset!.dx;
+      // Just perform the calculation for d instead of x
+
+      final double timeStampTime = (dx * audioLength) / size.width;
+      // Add the new timestamp to the list of timestamps
+      newTimeStamp.call(timeStampTime);
+
+      // Draw timestamp
+      final Offset p0 = Offset(dx, 0);
+      final Offset p1 = Offset(dx, size.height);
 
       canvas.drawLine(p0, p1, paint);
     }
+
+    // Remove hit timestamp(s)
+    hitTimeStamp.call(markedForRemoval);
   }
 
   @override
